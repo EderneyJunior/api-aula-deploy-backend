39ab77063694368fb9d01a3e6e6393a40a9d4028
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-10-18 11:12:49 -0300

    adicionado o script start para reroku

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ app.get('/', async (req, res) => {
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log(`Servidor rodando na porta: ${port}}`))
+app.listen(port, () => console.log(`Servidor rodando na porta: ${port}`))
