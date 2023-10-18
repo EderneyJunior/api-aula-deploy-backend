4e3f1dfbdc709c682baa1df09684949f77d05311
Ederney Pedroso Junior <pedrosoederney@gmail.com>
2023-10-18 13:26:49 -0300

    atualização

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 const knex = require('./conexao')
+const cors = require('cors')
 
 const app = express()
 
@@ -9,8 +9,12 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', async (req, res) => {
-    const carros = await knex('carros')
-    return res.json(carros)
+    try {
+        const carros = await knex('carros')
+        return res.json(carros)
+    } catch (error) {
+        return res.status(500).json({ menssagem: 'Erro no servidor' })
+    }
 })
 
 const port = process.env.PORT || 3000
